9ae166317bf8881bb0b21b02a5ca48f92358d610
Mohammad <mohammad.bagheri24680@gmail.com>
2022-09-06 02:20:41 +0430

    Fixed the Components and Stylesheet and Remove Unnecessary Things

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from "react";
+import {
+  actionsState,
+  optionsState,
+  penColorState,
+  penSizeState,
+} from "./initialStates/initialStates";
+
+import Header from "./components/header/header";
+
+function App() {
+  const [actions, setActions] = useState(actionsState);
+  const [penColor, setPenColor] = useState(penColorState);
+  const [options, setOptions] = useState(optionsState);
+
+  useEffect(() => {});
+
+  const changeActiveHandler = (id) => {
+    const cloneActions = [...actions];
+    cloneActions.forEach((item) => (item.action = false));
+
+    cloneActions.forEach((item) => {
+      if (item.id == id) item.action = true;
+    });
+
+    setActions(cloneActions);
+  };
+
+  const changeColorHandler = () => {};
+
+  return (
+    <>
+      <Header
+        actions={actions}
+        options={options}
+        changeColor={changeColorHandler}
+        changeActiveHandler={changeActiveHandler}
+      />
+    </>
+  );
+}
+
+export default App;
