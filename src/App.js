f54e6db79657ac5a5d8dc8cffa6abf61f663131c
officer-K99 <76909380+officer-K99@users.noreply.github.com>
2022-09-05 22:00:45 +0430

    add initial stated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import React, {useState,useEffect} from 'react';
+import {actionsState,optionsState,penColorState,penSizeState} from './initialStates/initialStates'
+
+import Header from './components/header/header'
 import './App.css';
 
 function App() {
+  const [actions,setActions] = useState(actionsState)
+  const [penColoe,setPenColor] = useState(penColorState)
+  const [penSize,setPenSize] = useState(penSizeState)
+  const [options,setOptions] = useState(optionsState)
+
+  useEffect(() => {
+
+  })
+
+
+
   return (
     <>
+      <Header />
     </>
   );
 }
---
+++ src/initialStates/initialStates.js
@@ -0,0 +1,36 @@
+export const actionsState = [{
+    name : 'Pencil',
+    imgeUrl : './adress',
+    action : false
+  },
+  {
+    name : 'Pen',
+    imgeUrl : './adress',
+    action : false
+  },
+  {
+    name : 'Eraser',
+    imgeUrl : './adress',
+    action : false
+  },
+  {
+    name : 'Cursor',
+    imgeUrl : './adress',
+    action : true
+},]
+
+export const optionsState = [{
+    name : 'color',
+    imageUrl : 'address',
+    action : false
+  },
+  {
+    name : 'Screen Shot',
+    imageUrl : 'address',
+    action : false
+  },
+]
+
+export const penColorState = '#000'
+
+export const penSizeState = '18px'
