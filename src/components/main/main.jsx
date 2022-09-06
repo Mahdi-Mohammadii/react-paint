10031bea6dcc139fee2c759420f83dfffcf32937
Mahdi-Mohammadii <mahdiprog81@gmail.com>
2022-09-06 23:02:44 +0430

    create constructor and state

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import "./main.css";
 export default class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { x: 0, y: 0 };
+  }
 
 }
 
