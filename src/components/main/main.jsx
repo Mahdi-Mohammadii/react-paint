ed303a9c28c86cd1d28179cedb063ea043982882
Mahdi-Mohammadii <mahdiprog81@gmail.com>
2022-09-06 23:04:14 +0430

    create function to set x and y in move

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -6,6 +6,12 @@ export default class Main extends React.Component {
     this.state = { x: 0, y: 0 };
   }
 
+  handleMouseMove(event) {
+    this.setState({
+      x: event.clientX,
+      y: event.clientY,
+    });
+  }
 }
 
 export default Main
