947e913a89f8d2b464a259228a7a330b71c2d34a
Mahdi-Mohammadii <mahdiprog81@gmail.com>
2022-09-06 23:18:20 +0430

    create cursor style

--- src/components/cursor/cursor.jsx
+++ src/components/cursor/cursor.jsx
@@ -3,4 +3,17 @@ export const Cursor = (props) => {
   const type = props.type;
   const color = props.color;
   const size = props.size;
+
+  let style = {
+    position: "absolute",
+    left: position.x,
+    top: position.y,
+    backgroundColor: color,
+    border: "none",
+    borderRadius: "0",
+    cursor: "none",
+    width: size,
+    height: size,
+    border: "1px solid black",
+  };
 };
