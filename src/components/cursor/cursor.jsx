738774e98eaef40a56603f92f40d7dfbe5b081cb
Mahdi-Mohammadii <mahdiprog81@gmail.com>
2022-09-09 16:55:18 +0430

    refactore: inline if statement

--- src/components/cursor/cursor.jsx
+++ src/components/cursor/cursor.jsx
@@ -1,22 +1,21 @@
 export const Cursor = ({ pos, type, color, size }) => {
-  let style = {
-    position: "absolute",
-    left: pos.x,
-    top: pos.y,
-    backgroundColor:
-      type === "Eraser"
-        ? "#fff"
-        : `rgba(${color.r},${color.g},${color.b},${color.a})`,
-    borderRadius: type === "Pencil" || type === "Pen" ? "50%" : "0",
-    cursor: "none",
-    width: size,
-    height: size,
-    border: "1px solid black",
-  };
-
-  if (type === "Cursor") {
-    style = { cursor: "default" };
-  }
+  let style =
+    type === "Cursor"
+      ? { cursor: "default" }
+      : {
+          position: "absolute",
+          left: pos.x,
+          top: pos.y,
+          backgroundColor:
+            type === "Eraser"
+              ? "#fff"
+              : `rgba(${color.r},${color.g},${color.b},${color.a})`,
+          borderRadius: type === "Pencil" || type === "Pen" ? "50%" : "0",
+          cursor: "none",
+          width: size,
+          height: size,
+          border: "1px solid black",
+        };
 
   return <div style={style}></div>;
 };
